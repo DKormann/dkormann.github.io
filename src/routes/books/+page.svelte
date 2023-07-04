0f88f1baab084396754406ab26df00ee4cc6ab4c
Dominik Kormann <dominikormann@outlook.com>
2023-07-04 11:34:45 +0200

    atlas

---
+++ src/routes/books/+page.svelte
@@ -0,0 +1,25 @@
+
+<div class=container>
+
+    <h2> some of my favourite books: </h2>
+
+    <!-- html list -->
+
+    <ul>
+
+        <li>
+            <a href="/books/atlas%20shrugged.html">Atlas Shrugged</a>
+        </li>
+    </ul>   
+
+</div>
+
+
+<style>
+
+    .container{
+        padding: 10vw;
+    }
+    
+
+</style>
